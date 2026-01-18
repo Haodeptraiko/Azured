675336bcfe8538039eaa6343bccf299c85f20578
Haodeptraiko <map766188@gmail.com>
2026-01-18 21:08:55 +0700

    Azured.lua

--- Azured.lua
+++ Azured.lua
@@ -25,7 +25,7 @@ ToggleBtn.Draggable = true
 UICorner.CornerRadius = UDim.new(1, 0)
 UICorner.Parent = ToggleBtn
 ToggleBtn.MouseButton1Click:Connect(function()
-    library:Close()
+    if library then library:Close() end
 end)
 
 local function GetTarget(fov)
@@ -55,7 +55,6 @@ local mt = getrawmetatable(game)
 local oldNamecall = mt.__namecall
 local oldIndex = mt.__index
 setreadonly(mt, false)
-
 mt.__namecall = newcclosure(function(self, ...)
     local args = {...}
     local method = getnamecallmethod()
@@ -70,7 +69,6 @@ mt.__namecall = newcclosure(function(self, ...)
     end
     return oldNamecall(self, ...)
 end)
-
 mt.__index = newcclosure(function(self, idx)
     if library.flags["SilentAimEnabled"] and self == Mouse and (idx == "Hit" or idx == "Target") and not checkcaller() then
         local T = GetTarget(library.flags["SilentAimFOV"])
@@ -83,28 +81,54 @@ mt.__index = newcclosure(function(self, idx)
 end)
 setreadonly(mt, true)
 
+local AimbotCircle = Drawing.new("Circle")
+local SilentCircle = Drawing.new("Circle")
+
 local LegitTab = library:AddTab("Legit"); 
 local LegitColunm1 = LegitTab:AddColumn();
 local LegitMain = LegitColunm1:AddSection("Aim Assist")
+LegitMain:AddDivider("Main");
 LegitMain:AddToggle{text = "Enabled", flag = "AimbotEnabled"}
-LegitMain:AddSlider{text = "Aimbot FOV", flag = "AimbotFov", min = 0, max = 750, value = 105}
+LegitMain:AddSlider{text = "Aimbot FOV", flag = "AimbotFov", min = 0, max = 750, value = 105, suffix = "°"}
+LegitMain:AddSlider{text = "Smoothing Factor", flag = "Smoothing", min = 0, max = 30, value = 3, suffix = "%"}
 LegitMain:AddList({text = "Hit Box", flag = "AimbotHitbox", value = "Head", values = {"Head", "Torso"}});
+LegitMain:AddList({text = "Aimbot Key", flag = "AimbotKey", value = "On Aim", values = {"On Aim", "On Shoot"}});
+LegitMain:AddDivider("Draw Fov");
+LegitMain:AddToggle{text = "Enabled", flag = "CircleEnabled"}:AddColor({flag = "CircleColor", color = Color3.new(1, 1, 1)});
+LegitMain:AddSlider{text = "Num Sides", flag = "CircleNumSides", min = 3, max = 48, value = 48, suffix = "°"}
 
 local LegitSecond = LegitColunm1:AddSection("Extend Hitbox")
+LegitSecond:AddDivider("Main");
 LegitSecond:AddToggle{text = "Enabled", flag = "HitboxEnabled"}
-LegitSecond:AddSlider{text = "Extend Rate", flag = "ExtendRate", min = 0, max = 15, value = 10}
+LegitSecond:AddList({text = "Hit Box", flag = "ExtendHitbox", value = "Head", values = {"Head", "Torso"}});
+LegitSecond:AddSlider{text = "Extend Rate", flag = "ExtendRate", min = 0, max = 10, value = 10, suffix = "%"}
+
+local LegitThird = LegitColunm1:AddSection("Trigger Bot")
+LegitThird:AddDivider("Main");
+LegitThird:AddToggle{text = "Enabled", flag = "TriggerEnabled"}:AddBind({flag = "TriggerBind", key = "One"});
+LegitThird:AddSlider{text = "Trigger Speed", flag = "TriggerSpeed", min = 0, max = 1000, value = 10, suffix = "%"}
 
 local LegitColunm2 = LegitTab:AddColumn();
 local LegitForth = LegitColunm2:AddSection("Bullet Redirection")
+LegitForth:AddDivider("Main");
 LegitForth:AddToggle{text = "Enabled", flag = "SilentAimEnabled"}
-LegitForth:AddSlider{text = "Silent Aim FOV", flag = "SilentAimFOV", min = 0, max = 750, value = 150}
-LegitForth:AddList({text = "Hit Box", flag = "SilentAimHitbox", value = "Torso", values = {"Head", "Torso"}});
+LegitForth:AddSlider{text = "Silent Aim FOV", flag = "SilentAimFOV", min = 0, max = 750, value = 105, suffix = "°"}
+LegitForth:AddSlider{text = "Hit Chances", flag = "HitChances", min = 0, max = 100, value = 100, suffix = "%"}
+LegitForth:AddList({text = "Redirection Mode", flag = "RedirectionMode", value = "P Mode", values = {"P Mode", "Normal Mode"}});
+LegitForth:AddList({text = "Hit Box", flag = "SilentAimHitbox", value = "Head", values = {"Head", "Torso"}});
+LegitForth:AddDivider("Draw Fov");
+LegitForth:AddToggle{text = "Enabled", flag = "Circle2Enabled"}:AddColor({flag = "Circle2Color", color = Color3.new(1, 1, 1)});
+LegitForth:AddSlider{text = "Num Sides", flag = "Circle2NumSides", min = 3, max = 48, value = 48, suffix = "°"}
+LegitForth:AddDivider("Checks");
+LegitForth:AddToggle{text = "Enabled", flag = "VisibleCheck"}
 
 local VisualsTab = library:AddTab("Visuals"); 
 local VisualsColunm2 = VisualsTab:AddColumn();
 local VisualsSecond = VisualsColunm2:AddSection("Camera Visuals")
+VisualsSecond:AddDivider("Main");
+VisualsSecond:AddToggle{text = "Enabled", flag = "CameraVisualsEnabled"}
 VisualsSecond:AddToggle{text = "Change Camera FOV", flag = "ChangeCameraFOV"}
-VisualsSecond:AddSlider{text = "Camera FOV", flag = "CameraFOV", min = 10, max = 120, value = 100}
+VisualsSecond:AddSlider{text = "Camera FOV", flag = "CameraFOV", min = 10, max = 120, value = 120, suffix = "°"}
 
 local SettingsTab = library:AddTab("Settings"); 
 local SettingsColumn = SettingsTab:AddColumn(); 
@@ -112,9 +136,24 @@ local SettingSection = SettingsColumn:AddSection("Menu");
 SettingSection:AddBind({text = "Open / Close", flag = "UI Toggle", nomouse = true, key = "End", callback = function() library:Close(); end});
 
 RunService.RenderStepped:Connect(function()
+    local Center = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
+    
+    AimbotCircle.Visible = library.flags["CircleEnabled"]
+    AimbotCircle.Position = Center
+    AimbotCircle.Radius = library.flags["AimbotFov"]
+    AimbotCircle.Color = library.flags["CircleColor"]
+    AimbotCircle.NumSides = library.flags["CircleNumSides"]
+
+    SilentCircle.Visible = library.flags["Circle2Enabled"]
+    SilentCircle.Position = Center
+    SilentCircle.Radius = library.flags["SilentAimFOV"]
+    SilentCircle.Color = library.flags["Circle2Color"]
+    SilentCircle.NumSides = library.flags["Circle2NumSides"]
+
     if library.flags["ChangeCameraFOV"] then
         Camera.FieldOfView = library.flags["CameraFOV"]
     end
+
     if library.flags["HitboxEnabled"] then
         local size = library.flags["ExtendRate"]
         for _, v in pairs(Players:GetPlayers()) do
